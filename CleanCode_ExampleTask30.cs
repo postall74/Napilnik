d9155f4e4e29805603a3ded831b16ccf55133053
postall74 <postall_mcs@mail.ru>
2022-07-21 23:43:03 +0500

    Create CleanCode_ExampleTask30.cs

---
+++ CleanCode_ExampleTask30.cs
@@ -0,0 +1,24 @@
+﻿class Switcher
+{
+    private bool _enable;
+
+    public void SwitchOn(bool enable)
+    {
+        _enable = enable;
+
+        if (enable)
+            _effects.StartEnableAnimation();
+        else
+            SwitchOff(enable);
+    }
+
+    public void SwitchOff(bool enable)
+    {
+        _enable = enable;
+
+        if (!enable)
+            _pool.Free(this);
+        else
+            SwitchOn(enable);
+    }
+}
